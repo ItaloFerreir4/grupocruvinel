1dc615b3ede4eca348239ceeb6a9f71bb2456319
Caio Andreta <caioandreta@gmail.com>
2024-02-29 19:33:02 -0300

    front end de empresa detalhes concluído

--- empresas.php
+++ empresas.php
@@ -11,8 +11,6 @@ include_once("./assets/componentes.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nossas Empresas</title>
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/empresas.css">
